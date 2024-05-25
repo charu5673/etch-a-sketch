34464e5df598b3cee5d77c897cc794a5e6f3d017
Charu <charumishra5673@gmail.com>
2024-05-25 13:26:39 +0530

    sketchpad complete

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let cont = document.querySelector("#container");
+n=16;
+generate(n);
+function generate(n)
+{
 let i,j;
-for(i=0;i<16;i++)
+for(i=0;i<n;i++)
     {
         let d=document.createElement("div");
         d.classList.add("outer");
-        for(j=0;j<16;j++)
+        for(j=0;j<n;j++)
             {
                 let di=document.createElement("div");
                 di.classList.add("inner");
@@ -16,3 +20,24 @@ for(i=0;i<16;i++)
         if(e.target.className=="inner")
             e.target.style.backgroundColor="black";
     });
+}
+function change()
+{
+    let x=parseInt(prompt("Enter new size of grid (nxn)\nMaximum: 100\nMinimum:1"));
+    if(x===NaN)
+        {
+            prompt("INVALID!");
+            change();
+        }
+    else if(x<1)
+        x=1;
+    else if(x>100)
+        x=100;
+    n=x;
+    reset();
+}
+function reset()
+{
+    cont.innerHTML="";
+    generate(n);
+}
